3d23e36778173f9099083b18fa9e402cb469c4b8
ARIEL <carraro.ariel@gmail.com>
2020-04-18 11:08:26 -0500

    App.java editado

--- Introduccion/src/main/java/com/cursospring/main/App.java
+++ Introduccion/src/main/java/com/cursospring/main/App.java
@@ -3,6 +3,17 @@ package com.cursospring.main;
 
 public class App {
     public static final String MENSAJE = "Bienvenido al mundo Spring !!";
+
+    private ISaludo saludo;
+    
+    public App(ISaludo saludo){
+      this.saludo = saludo;
+    }
+    
+    public void mensaje(){
+      
+    }
+
     public static void main( String[] args ){
         System.out.println(MENSAJE);
     }
