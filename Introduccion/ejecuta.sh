1c0ecab48920939f30661915367e16a1a4ccad1c
Fer_Carraro <carraro.fernando@gmail.com>
2020-04-18 10:30:08 -0500

    Se han creado ahrcivos: ejecuta.sh y crearproyecto.sh

---
+++ Introduccion/ejecuta.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Compilando
+mvn compile
+echo Ejecutando
+mvn exec:java -Dexec.mainClass="com.cursospring.main.App"  
---
+++ crearproyecto.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Creando proyecto Spring-Maven
+mvn archetype:generate -DgroupId=com.cursospring.main -DartifactId=Introduccion -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
