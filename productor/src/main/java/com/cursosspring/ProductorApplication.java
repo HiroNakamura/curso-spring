7cde4cbb16f1873a830d9a8d45a8bd900de9fd90
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-01 16:21:19 -0600

    Se creo proyecto servidor

---
+++ productor/src/main/java/com/cursosspring/ProductorApplication.java
@@ -0,0 +1,13 @@
+package com.cursosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductorApplication.class, args);
+	}
+
+}
