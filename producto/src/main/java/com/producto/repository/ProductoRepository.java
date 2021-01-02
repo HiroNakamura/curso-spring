0a395a10c953c04563ab53c9e570c76e4b8b0ba1
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-02 13:07:31 -0600

    Se elimino proyecto producto

--- producto/src/main/java/com/producto/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.producto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.producto.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long>{
-}
