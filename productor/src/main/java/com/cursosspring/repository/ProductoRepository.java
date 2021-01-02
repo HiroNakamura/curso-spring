164622560902c626fb25c4326327b2c2daf42108
Fer_Carraro <carraro.fernando@gmail.com>
2021-01-01 18:15:25 -0600

    Se ha creado entidad y repositorio para Producto

---
+++ productor/src/main/java/com/cursosspring/repository/ProductoRepository.java
@@ -0,0 +1,8 @@
+package com.cursospring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cursospring.model.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+
+}
